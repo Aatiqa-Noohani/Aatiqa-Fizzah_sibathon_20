e1052df9e88e49db0bd1c9c67541593cc08e2d1f
fizzahrajput604 <fizzahrajput729@gmail.com>
2026-02-13 03:12:08 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-import strreamlit as st
-import pandas as pd 
-import plotly.express as px 
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 from model import EnergyAI
 
-###Configuration & UI Theme
+# --- CONFIGURATION & UI THEME ---
 st.set_page_config(page_title="Smart Energy AI", layout="wide")
 
-## Styling With CSS
-st.markdown("""<style>
-.main{background-color:#1f2937;color:white;}
+# Classic UI Styling with CSS
+st.markdown("""
+    <style>
+    .main { background-color: #0e1117; color: white; }
+    .stMetric { background-color: #1f2937; padding: 15px; border-radius: 10px; border-left: 5px solid #3b82f6; }
+    div[data-testid="stExpander"] { border: none; background-color: #1f2937; border-radius: 10px; }
+    </style>
+    """, unsafe_allow_html=True)
 
-
-
-</style>""")
+# --- BACKEND CONNECTIVITY ---
+# Initializing the AI model with the local CSV
+ai = EnergyAI('data/house_hold_consumption.csv')
+v_mean, p_total, co2 = ai.calculate_metrics()
